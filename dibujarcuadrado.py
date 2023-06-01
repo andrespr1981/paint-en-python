62d9c76fce5360e8eac1ef889f3895bc6bb88c61
andrespr1981 <118039027+andrespr1981@users.noreply.github.com>
2023-05-31 21:24:45 -0700

    Add files via upload

---
+++ dibujarcuadrado.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.locals import*
+
+AzulCielo=(63,255,238)
+Blanco=(255, 255, 255)
+Negro=(0, 0, 0)
+Rojo=(255, 0, 0)
+Verde=(0, 255, 0)
+Azul=(0, 0, 255)
+
+SCREEN_AL=1100
+SCREEN_AN=600
+SCREEN=pygame.display.set_mode((SCREEN_AL,SCREEN_AN))
+SCREEN.fill(Blanco)
+
+def hacercuadrado(c,g,x1,y1):
+	if c == 1:
+		color=AzulCielo
+	if c == 2:
+		color=Blanco
+	if c == 3:
+		color=Negro
+	if c == 4:
+		color=Rojo
+	if c == 5:
+		color=Verde
+	if c == 6:
+		color=Azul
+	y2=y1+100#Que tan largo va a ser el cuadrado
+	x2=x1
+	pygame.draw.line(SCREEN, color, (x1,y1),(x2,y2), g)
